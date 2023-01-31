4201499ad47e2038521abe143c0f1d5113320868
Mohamed Ahmed <41699093+Mo-Ali98@users.noreply.github.com>
2023-01-31 22:36:47 +0000

    adjust head title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <title>Generate Code!</title>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
