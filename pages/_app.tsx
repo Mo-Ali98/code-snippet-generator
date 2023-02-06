8f1df87de5ae8182465edb2fecd1378bfb39e271
Mohamed Ahmed <41699093+Mo-Ali98@users.noreply.github.com>
2023-02-06 23:05:52 +0000

    Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { AppProvider } from "../contexts/app-context";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppProvider>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </AppProvider>
   );
